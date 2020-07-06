e933e704e44ac4dbe4ab2f31a471a243bff254de
Samuelford24 <Samuelford48@gmail.com>
2020-07-06 15:59:21 -0400

    update widget

---
+++ app/src/main/java/Widget/AnnouncementService.java
@@ -0,0 +1,85 @@
+package Widget;
+
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.samuelford48gmail.campnavigateandroid.Announcement;
+import com.samuelford48gmail.campnavigateandroid.R;
+
+import java.util.ArrayList;
+
+public class AnnouncementService extends IntentService {
+    public final static String ACTION_RELOAD_ANNOUNCEMENTS = "com.samuelford48gmail.campnavigateandroid.Widget.reload_announcements";
+    public final static String ACTION_UPDATE_LISTVIEW = "com.samuelford48gmail.campnavigateandroid.Widget.update_lv";
+    ArrayList<Announcement> announcements;
+
+    public AnnouncementService(String name) {
+        super(name);
+    }
+
+    public static void startReloadService(Context context) {
+        Intent intent = new Intent(context, AnnouncementService.class);
+        intent.setAction(ACTION_RELOAD_ANNOUNCEMENTS);
+
+        context.startService(intent);
+
+    }
+
+    public static void startupdateListView(Context context) {
+        Intent intent = new Intent(context, AnnouncementService.class);
+        intent.setAction(ACTION_UPDATE_LISTVIEW);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent.getAction().equals(ACTION_RELOAD_ANNOUNCEMENTS)) {
+            reloadAnnouncements();
+        } else if (intent.getAction().equals(ACTION_UPDATE_LISTVIEW)) {
+            updateListView();
+        }
+    }
+
+    private void updateListView() {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, AnnouncementWidget.class));
+        //Trigger data update to handle the GridView widgets and force a data refresh
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widgetLV);
+        //Now update all widgets
+        AnnouncementWidget.updateAnnouncementWidgets(this, appWidgetManager, appWidgetIds);
+
+    }
+
+
+    private void reloadAnnouncements() {
+        announcements = new ArrayList<>();
+        FirebaseFirestore.getInstance().collection("Announcements").orderBy("SortTimeStamp", Query.Direction.DESCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    //error
+                }
+                announcements.clear();
+                for (DocumentSnapshot d : queryDocumentSnapshots) {
+                    announcements.add(new com.samuelford48gmail.campnavigateandroid.Announcement((String) d.get("announcement"), (String) d.get("timeStamp")));
+
+
+                }
+
+            }
+
+        });
+    }
+}
--- app/src/main/java/Widget/AnnouncementWidget.java
+++ app/src/main/java/Widget/AnnouncementWidget.java
@@ -5,6 +5,9 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
+import android.util.Log;
+import android.widget.ImageButton;
 import android.widget.RemoteViews;
 
 import com.samuelford48gmail.campnavigateandroid.MainActivity;
@@ -14,30 +17,55 @@ import com.samuelford48gmail.campnavigateandroid.R;
  * Implementation of App Widget functionality.
  */
 public class AnnouncementWidget extends AppWidgetProvider {
+    // WIDGET_BUTTON
+    public static String WIDGETBUTTON = "com.samuelford48gmail.campnavigateandroid.Widget.WIDGET_BUTTON";
+    ImageButton imageButton;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-    ///    CharSequence widgetText = context.getString(R.string.appwidget_text);
+        appWidgetManager.updateAppWidget(appWidgetId, getView(context));
+    }
+
+    public static void updateAnnouncementWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private static RemoteViews getView(Context context) {
+        ///    CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.announcement);
-       // views.setTextViewText(R.id.appwidget_text, widgetText);
-        Intent intent = new Intent(context,AnnouncementWidgetService.class);
+        // views.setTextViewText(R.id.appwidget_text, widgetText);
+        Intent intent = new Intent(context, AnnouncementWidgetService.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setPendingIntentTemplate(R.id.widgetLV, pendingIntent);
+        Intent intent2 = new Intent(WIDGETBUTTON);
+        PendingIntent pendingIntent2 = PendingIntent.getBroadcast(context, 0, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.reload, pendingIntent2);
+        views.setRemoteAdapter(R.id.widgetLV, intent);
 
-        views.setRemoteAdapter(R.id.widgetLV,intent);
 
-
-        views.setPendingIntentTemplate(R.id.widgetLV,pendingIntent);
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        // appWidgetManager.updateAppWidget(appWidgetId, views);
+        return views;
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        //Start the intent service update widget action, the service takes care of updating the widgets UI
+        AnnouncementService.startReloadService(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.i("onReceive", "Received");
+        if (WIDGETBUTTON.equals(intent.getAction())) {
+            AnnouncementService.startReloadService(context);
+//your code here
+
         }
     }
 
